73cb9516653af6578786ba357ab79e855167a29f
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-08 10:36:28 -0500

    added products to data base

---
+++ rainforest/rainforest/admin.py
@@ -0,0 +1,4 @@
+from rainforest.models import Product
+from django.contrib import admin
+
+admin.site.register(Product)
--- rainforest/rainforest/models.py
+++ rainforest/rainforest/models.py
@@ -4,3 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
+
+
+    def __str__(self):
+        return "Name: {} Price: {} Description: {}".format(self.name, self.price, self.description)
