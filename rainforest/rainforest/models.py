413c397ac5c4c5c464f6d00d5a5290eb4fbaedbb
Ben Lardie <benlardie@gmail.com>
2019-03-08 17:16:00 -0500

    price in dollars is up and running

--- rainforest/rainforest/models.py
+++ rainforest/rainforest/models.py
@@ -9,3 +9,8 @@ class Product(models.Model):
 
     def __str__(self):
         return "Name: {} Price: {} Description: {}".format(self.name, self.price, self.description)
+
+
+    def price_in_dollars(self):
+        dollars = self.price / 100
+        return "${:.2f}".format(dollars)
